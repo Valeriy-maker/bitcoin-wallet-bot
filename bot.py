b4f48bec4c08bb1cce53e1f8253a271241367b4c
Valeriy-maker <kirillov250276k@gmail.com>
2025-05-10 22:59:37 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -50,7 +50,7 @@ async def receive_txid(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(" Checking transaction...")
 
     if check_transaction(txid):
-        await update.message.reply_text("✅ Payment confirmed!
+        await update.message.reply_text(" Payment confirmed!
 Here is your Bitcoin wallet password:
 
 *bitpass-XYZ123456*", parse_mode='Markdown')
